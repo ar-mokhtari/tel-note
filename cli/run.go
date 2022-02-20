9461350e331768d73c46734191f75f95ad23adf5
HQ2-AlirezaMokhtariG <neatland@gmail.com>
2022-02-20 23:11:06 +0330

    14001201->HQ2->AddCliToRun->menuClean

---
+++ cli/run.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	"tel-note/internal/config"
+	"tel-note/internal/service/identity"
+)
+
+func RunApp() {
+	//serve port 1212
+	//cli.Serv()
+
+	//To know level of user (admin | normal user)
+	Identity()
+
+	//If user is admin, maybe want to change greeting note
+	if identity.IsAdmin {
+		ChangeGreeting()
+	}
+
+	//Show greeting note and description
+	ShowGreeting()
+
+	//create global MainData
+	config.Init()
+
+	//show contact menu
+	ShowMenu()
+}
--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"tel-note/cli"
-	"tel-note/internal/config"
-	"tel-note/internal/service/identity"
-)
+import "tel-note/cli"
 
 func main() {
-	//serve port 1212
-	//cli.Serv()
-
-	//To know level of user (admin | normal user)
-	cli.Identity()
-
-	//If user is admin, maybe want to change greeting note
-	if identity.IsAdmin {
-		cli.ChangeGreeting()
-	}
-
-	//Show greeting note and description
-	cli.ShowGreeting()
-
-	//create global MainData
-	config.Init()
-
-	//show contact menu
-	cli.ShowMenu()
+	cli.RunApp()
 }
