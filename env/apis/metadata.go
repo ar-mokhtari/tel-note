fdc5e8537572ccf3906b8bd08218f6144edd6e86
HQ2-AlirezaMokhtariG <neatland@gmail.com>
2022-03-08 14:55:19 +0330

    14001217->HQ2->API->Pending

--- env/apis/metadata.go
+++ env/apis/metadata.go
@@ -7,7 +7,7 @@ package apis
 const (
 	//UniversalTutorial
 	UniversalTutorialURL    = "https://www.universal-tutorial.com/api/countries"
-	UniversalTutorialAPIKey = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJuZWF0bGFuZEBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJQLUJnMVVOM24tbzdPczNETFZoUWxFVUlXWENaQUw5X3Q2SXFFMDhwYlhOb0ZTdGpHWXN2TVlBZTJ1UFc3U1V3UkRzIn0sImV4cCI6MTY0NjgwOTUxOH0.sYxbcDY4Lz-SEKlAx9eyRTFM3ICZiJFac3In6rbQStg"
+	UniversalTutorialAPIKey = ""
 
 	//Neshan
 	NeshanURL    = ""
