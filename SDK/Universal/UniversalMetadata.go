1985bd5248cb209d88f16923dbdd662d352d41ad
ar_mokhtari_g <neatland@gmail.com>
2022-03-12 13:27:42 +0330

    14001221->HQ1->SDK->NewStructure

---
+++ SDK/Universal/UniversalMetadata.go
@@ -0,0 +1,10 @@
+package Universal
+
+const (
+
+	// GetCountryURL UniversalTutorialURL UniversalTutorial
+	GetCountryURL          = "https://www.universal-tutorial.com/api/countries"
+	GetTokenURL            = "https://www.universal-tutorial.com/api/getaccesstoken"
+	UniversaltutorialToken = "P-Bg1UN3n-o7Os3DLVhQlEUIWXCZAL9_t6IqE08pbXNoFStjGYsvMYAe2uPW7SUwRDs"
+	Email                  = "neatland@gmail.com"
+)
