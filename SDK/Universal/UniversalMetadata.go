b77f3b61074d6a2eccdbdb491ac302aed5d4dbfa
Alireza Mokhtari Garakani <49469395+ar-mokhtari@users.noreply.github.com>
2022-03-27 11:23:20 +0430

    Create UniversalMetadata.go

---
+++ SDK/Universal/UniversalMetadata.go
@@ -0,0 +1,11 @@
+package Universal
+
+const (
+	// Apply to get services from this site:
+	//https://www.universal-tutorial.com/
+	// GetCountryURL UniversalTutorialURL UniversalTutorial
+	GetCountryURL          = "https://www.universal-tutorial.com/api/countries"
+	GetTokenURL            = "https://www.universal-tutorial.com/api/getaccesstoken"
+	UniversaltutorialToken = "your_token"
+	Email                  = "your_email"
+)
