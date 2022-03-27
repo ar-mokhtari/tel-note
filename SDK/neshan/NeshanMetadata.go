0333e87a531c9c64eb10df1fdb971ba14763e86d
ar_mokhtari_g <neatland@gmail.com>
2022-03-27 12:12:00 +0430

    [.gitignore]->git rm --cached .gitignore

---
+++ SDK/neshan/NeshanMetadata.go
@@ -0,0 +1,10 @@
+package neshan
+
+const (
+	//Apply to get services from this site:
+	//https://platform.neshan.org/api/getting-started
+
+	//DistanceMatrixURL Neshan
+	DistanceMatrixURL = "https://api.neshan.org/v1/distance-matrix"
+	ApiKey            = token
+)
