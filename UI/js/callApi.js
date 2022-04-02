b9bf381f59ed867988b3893f947c95ee136e89ca
HQ2-AlirezaMokhtariG <neatland@gmail.com>
2022-04-02 17:06:06 +0430

    [UI]->improve->side bar->pending

--- UI/js/callApi.js
+++ UI/js/callApi.js
@@ -2,9 +2,11 @@ const addContactUrl = 'http://127.0.0.1:1212/new-contact';
 const addCityUrl = 'http://127.0.0.1:1212/add-city';
 const fillDataUrl = 'http://127.0.0.1:1212/fill-data';
 const getDataUrl = 'http://127.0.0.1:1212/get-data';
+const getMenuUrl = 'http://127.0.0.1:1212/menu-list';
+
 const newCityData = {
-    "Name": "شهر قل قل",
-    "EnglishName": "ghol ghol town",
+    "Name": "شهر جدید",
+    "EnglishName": "new town",
     "AriaCode": "131313",
     "Lat": "36.5678161",
     "Lng": "51.8789308"
@@ -26,7 +28,7 @@ function getData() {
         dataType: "json",
         data: JSON.stringify({CityData: "CityData", ContactData: "ContactData"}),
         success: function (data) {
-            $("#response span").append("<table data-toggle='table' class='table table-striped table-hover table-bordered caption-top table-responsive table-sm'><caption>City data</caption></table>");
+            $("#response span").append("<table data-toggle='table' class='table table-striped table-hover table-responsive small'></table>");
             $("#response span table").append("<thead><tr class='table-primary'><td>Name</td><td>EnglishName</td><td>AriaCode</td><td>Lat</td><td>Lng</td></tr></thead><tbody></tbody>");
             $.each(data.CityData, function (index, element) {
                 $("#response span table tbody").append("<tr>" + "<td>" + (JSON.stringify(element.Name)) + '</td>' +
@@ -36,9 +38,7 @@ function getData() {
                     "<td>" + (JSON.stringify(element.Lng)) + '</td>' +
                     "</tr>");
             });
-            $("#response span").append("<hr>");
-            $("#response span").append("<span>"
-                + JSON.stringify(data.ContactData) + '</span>');
+            // $("#response span").append("<span>" + JSON.stringify(data.ContactData) + '</span>');
         }
     });
 }
@@ -79,6 +79,40 @@ function addContact() {
     });
 }
 
+function getMenu() {
+    $.ajax({
+        type: 'GET',
+        url: getMenuUrl,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify({GroupName: "GroupName", Row: "Row"}),
+        Row: JSON.stringify({Title: "Title", Description: "Description"}),
+        success: function (data) {
+            $("#MainSidebar ul").empty()
+            $.each(data, function (index, element) {
+                $("#MainSidebar ul").append(
+                    "<li class='mb-1'>" +
+                    "<button class=\"btn btn-toggle align-items-center rounded collapsed\" data-bs-toggle=\"collapse\"\n" +
+                    "data-bs-target=\"" + "#group-collapse" + index + "\" aria-expanded=\"false\">\n" +
+                    element.GroupName +
+                    "</button>" +
+                    "<div class=\"collapse\" id=\"" + "group-collapse" + index + "\">\n" +
+                    "    <ul class=\"btn-toggle-nav list-unstyled fw-normal pb-1 small\">\n" +
+                    "<li><a href=\"#\" class=\"link-dark rounded\">" + JSON.stringify(element.Row) + "</a></li>" +
+                    // $.each(element.Row, function (detailIndex, detailElement) {
+                    //     console.log(detailElement.Description)
+                    //     // alert(JSON.stringify(detailElement.Description))
+                    //     // this.append("<li><a href=\"#\" class=\"link-dark rounded\">" + JSON.stringify(detailElement.Description) + "</a></li>")
+                    // })
+                    "    </ul>\n" +
+                    "</div>" +
+                    "</li>" +
+                    "</ul>"
+                )
+            });
+        }
+    });
+}
 
 $('.getDataBtn').click(function () {
     getData()
@@ -90,6 +124,7 @@ $('.addCityBtn').click(function () {
     addCity()
 })
 $('.addContactBtn').click(function () {
-    addContact()
+    // addContact()
+    getMenu()
 })
 
--- cli/menuHandle.go
+++ cli/menuHandle.go
@@ -6,7 +6,10 @@ import (
 )
 
 func menuHandle(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	json.NewEncoder(w).Encode(GlobalMenu.Group)
 }
 
