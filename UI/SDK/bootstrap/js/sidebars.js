1b2af0d4636781f1a69130b4abb3af8b8d094087
ar_mokhtari_g <neatland@gmail.com>
2022-04-09 14:52:54 +0430

    [UI]->Updated

--- UI/SDK/bootstrap/js/sidebars.js
+++
@@ -1,8 +0,0 @@
-/* global bootstrap: false */
-(function () {
-    'use strict'
-    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-        new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-})()
