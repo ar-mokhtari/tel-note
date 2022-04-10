b4f5e37d209a8af3817d9e0828d91e4e7168019d
ar_mokhtari_g <neatland@gmail.com>
2022-04-10 09:08:55 +0430

    [menu]->added menu type to manege menu in front-end

---
+++ UI/services/grids.js
@@ -0,0 +1,17 @@
+function createGrids(subMenu) {
+    $("#MainGrid").empty();
+    $("#MainGrid").append(
+        "       <!-- Tabs -->\n" +
+        "        <ul id=\"mainTab\" class=\"nav nav-tabs\">\n" +
+        "            <li class=\"nav-item\"><a class=\"nav-link active\" href=\"#ResponseTabContent\">" + subMenu + "</a></li>\n" +
+        "        </ul>\n" +
+        "        <!-- Tabs -->\n" +
+        "\n" +
+        "        <!-- Response body -->\n" +
+        "        <div id=\"tab-content\" class=\"tab-content\">\n" +
+        "            <div class=\"tab-pane fade show active\" id=\"ResponseTabContent\">\n" +
+        "                <div></div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <!-- Response body -->\n");
+}
